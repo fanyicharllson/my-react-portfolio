fd07645f466490f3648f80d950b22d27c17ca6f2
fanyicharllson <fanyicharllson1@gmail.com>
2025-01-01 16:02:36 -0800

    Refactor App component to include Navbar and update structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-purple-500 flex justify-center items-center h-screen">
-      Hello world!
-    </h1>
+   <main>
+    <Navbar />
+   </main>
   )
 }
